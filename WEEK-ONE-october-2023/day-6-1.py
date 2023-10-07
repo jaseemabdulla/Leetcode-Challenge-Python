8f9edbcae90c2df85669ec04ecd83162aa5609f1
jaseem <jaseempk003@gmail.com>
2023-10-07 11:46:44 +0530

    oct-23 week one day 6

---
+++ WEEK-ONE-october-2023/day-6-1.py
@@ -0,0 +1,33 @@
+# 1507. Reformat Date
+
+#Given a date string in the form Day Month Year, where:
+
+# Day is in the set {"1st", "2nd", "3rd", "4th", ..., "30th", "31st"}.
+# Month is in the set {"Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"}.
+# Year is in the range [1900, 2100].
+# Convert the date string to the format YYYY-MM-DD, where:
+
+# YYYY denotes the 4 digit year.
+# MM denotes the 2 digit month.
+# DD denotes the 2 digit day.
+
+class Solution(object):
+    def reformatDate(self, date):
+        months = {
+            'Jan' : '01',
+            'Feb' : '02',
+            'Mar' : '03',
+            'Apr' : '04',
+            'May' : '05',
+            'Jun' : '06',
+            'Jul' : '07',
+            'Aug' : '08',
+            'Sep' : '09',
+            'Oct' : '10',
+            'Nov' : '11',
+            'Dec' : '12',
+        }
+        day, month, year  = date.split()
+        day = day[:-2]
+        day = '0'+day if len(day) == 1 else day
+        return year+"-"+months[month]+"-"+day
